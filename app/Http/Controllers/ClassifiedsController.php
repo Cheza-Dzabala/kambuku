5ca290572a57608d8d652c3f78663e2dfe3c732f
Cheza <dzabalamacheza@gmail.com>
2016-09-05 11:56:13 +0200

    Resizing Image Limit to 7MB

--- app/Http/Controllers/ClassifiedsController.php
+++ app/Http/Controllers/ClassifiedsController.php
@@ -341,7 +341,7 @@ class ClassifiedsController extends Controller
        // dd($watermark_dir);
         foreach ($files as $file) {
             if (!empty($file)) {
-                $rules = array('file' => 'required|mimes:jpeg,jpg,png|max:2000');
+                $rules = array('file' => 'required|mimes:jpeg,jpg,png|max:7000');
                 $validator = validator(array('file' => $file), $rules);
                 if ($validator->passes()){
                     $img = Image::make($file);
