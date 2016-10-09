a6b3ab7bce3d4af400ee450fbe721c63a7e2e5f6
Cheza <dzabalamacheza@gmail.com>
2016-10-09 18:24:34 +0200

    Image Paths

--- app/Http/Controllers/App/AppdashBoardController.php
+++ app/Http/Controllers/App/AppdashBoardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\App;
 
 use App\classifieds;
+use App\Config;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -14,15 +15,25 @@ class AppdashBoardController extends Controller
     //
     public function index()
     {
-        $listings = classifieds::orderBy('id', 'DESC')->whereIs_active('1')->take(20)->get()->toJson();
+        $listings = classifieds::orderBy('id', 'DESC')->whereIs_active('1')->take(20)->get();
 
-        return $listings;
+        $config = Config::whereName('image_dir')->first();
+
+        foreach ($listings as $listing)
+        {
+            $listing->image_path = $config->value.$listing->image_path;
+        }
+
+
+        return $listings->toJson();
 
     }
 
     public function userDetails($id)
     {
         $listing = classifieds::whereId($id)->first();
+
+
         $userDetails = User::whereId($listing->user_id)->first();
         return $userDetails->toJson();
     }
