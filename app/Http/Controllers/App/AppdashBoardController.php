f8f20980a4275fa01bc02cfa0958c4ab7e2dbcf6
Cheza-Dzabala <dzabalamacheza@gmail.com>
2017-03-27 23:40:22 +0200

    App Featured

--- app/Http/Controllers/App/AppdashBoardController.php
+++ app/Http/Controllers/App/AppdashBoardController.php
@@ -32,6 +32,7 @@ class AppdashBoardController extends Controller
     public function featured()
     {
         $listings = classifieds::orderBy('id', 'DESC')
+            ->whereIs_active('1')
             ->whereIs_featured('1')
             ->take(20)->get();
 
