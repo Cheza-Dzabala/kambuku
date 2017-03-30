2f8ad9ab105c5e10e9080ce9a7297c5c15f73788
Cheza-Dzabala <dzabalamacheza@gmail.com>
2017-03-30 20:41:08 +0200

    App Search

--- app/Http/Controllers/App/appSearchController.php
+++ app/Http/Controllers/App/appSearchController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\App;
 
 use App\classifieds;
+use App\Config;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -14,10 +15,18 @@ class appSearchController extends Controller
 
     public function search($searchTerm)
     {
+        $config = Config::whereName('image_dir')->first();
+
         $results = classifieds::where('title', 'LIKE', '%' . $searchTerm . '%')
             ->orWhere('description', 'LIKE', '%' . $searchTerm . '%')
             ->orWhere('keywords', 'LIKE', '%' . $searchTerm . '%')
             ->get();
+
+
+        foreach ($results as $result)
+        {
+            $result->image_path = $config->value.$result->image_path;
+        }
 
         return $results->toJson();
     }
